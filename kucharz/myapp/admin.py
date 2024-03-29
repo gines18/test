d64dd769f75a3ae384d59824ecd720c58d7935e8
Mariusz Ruczkowski <mario@Mariuszs-Mini.lan>
2024-03-29 11:32:09 +0000

    add Reservation model

--- kucharz/myapp/admin.py
+++ kucharz/myapp/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from .models import Logger
+from .models import Logger, Reservation
 
 # Register your models here.
 admin.site.register(Logger)
+admin.site.register(Reservation)
 
--- kucharz/myapp/forms.py
+++ kucharz/myapp/forms.py
@@ -7,7 +7,12 @@ class LogForm(forms.ModelForm):
     class Meta:
         model = Logger
         fields = '__all__'
-      
+        widgets = {
+            'email': forms.EmailInput(attrs={'placeholder': 'example@example.com'}),
+            'message': forms.TextInput(attrs={'placeholder': 'Enter message'}),
+            'date': forms.DateInput(attrs={'type': 'date'}),
+            'time': forms.TimeInput(attrs={'type': 'time'}),
+        }
   
    
   
