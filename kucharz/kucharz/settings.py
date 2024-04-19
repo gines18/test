48a1d11fa9f68b9db916b1c3f26e6b6d8a992d11
Mariusz Ruczkowski <mario@Mariuszs-Mini.lan>
2024-04-19 16:53:55 +0100

    update static_root

--- kucharz/kucharz/settings.py
+++ kucharz/kucharz/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-)o8_!m*5p2#e#0*0xj^@25obs*oy@diw0g3t(a91rl4lb_tio8'
 
-# SECURITY WARNING: don't run with debug turned on in production!
+# SECURITY WARNING: don't run with debug turned on ˙∆ production!
 DEBUG = True
 
 ALLOWED_HOSTS = ['gines18.pythonanywhere.com']
@@ -117,7 +117,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = '/Users/mario/bootcamp/test/kucharz/static'
+STATIC_ROOT = 'staticfiles'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
